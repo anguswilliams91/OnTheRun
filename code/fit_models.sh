700d4d01e67a8cbf6dbf764fb775bc9ca96dec74
anguswilliams91 <anguswilliams91@gmail.com>
2016-11-15 13:08:02 +0000

    add convenient script

---
+++ code/fit_models.sh
@@ -0,0 +1,3 @@
+nice -n 10 python models.py "spherical_powerlaw" "/data/aamw3/mcmc/escape_chains/spherical_powerlaw.dat" "all" 80 20 5000
+nice -n 10 python models.py "flattened_powerlaw" "/data/aamw3/mcmc/escape_chains/flattened_powerlaw.dat" "all" 80 20 5000
+nice -n 10 python models.py "TF" "/data/aamw3/mcmc/escape_chains/TF.dat" "all" 80 20 5000
